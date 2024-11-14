46f94d5001f6f998add94d8f758e5430840c2309
harshithaa <59165053+sowmyahd2@users.noreply.github.com>
2024-11-14 13:06:16 +0530

    ds

--- src/Container/Home.js
+++ src/Container/Home.js
@@ -31,9 +31,7 @@ const Home = () => {
     const {departments} = useSelector(state => state.department);
 
 console.log([departments]);
-const mostViewProduct = useSelector(state => state.Product.mostView);
-const mostViewStore = useSelector(state => state.Store.mostView);
-const offersBrand = useSelector(state => state.Department.department);
+
     return ( 
         <Suspense fallback={<Loader />} >
         <Header />
@@ -43,9 +41,7 @@ const offersBrand = useSelector(state => state.Department.department);
                     <Suspense fallback={<Loader />} ><Homeslider /></Suspense>
                     <Suspense fallback={<Loader />} ><Banner /></Suspense>
                     <Suspense fallback={<Loader />} ><Vertical /></Suspense>
-                    <Suspense fallback={<Loader />} ><Cauroselproduct data={mostViewProduct} product={true} /></Suspense>
-                    <Suspense fallback={<Loader />} ><Cauroselstore data={mostViewStore} store={true} /></Suspense>
-                    <Suspense fallback={<Loader />} ><Cauroselbrand data={offersBrand} offer={true} /></Suspense>
+                   
                     </div>
                 </div>
             </div>
