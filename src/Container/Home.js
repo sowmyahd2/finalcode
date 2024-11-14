26c2834ef6fb3b31c3fa76eb53d233bde9ad520c
harshithaa <59165053+sowmyahd2@users.noreply.github.com>
2024-11-14 12:58:07 +0530

    sa

--- src/Container/Home.js
+++ src/Container/Home.js
@@ -41,9 +41,7 @@ console.log([departments]);
                     <Suspense fallback={<Loader />} ><Homeslider /></Suspense>
                     <Suspense fallback={<Loader />} ><Banner /></Suspense>
                     <Suspense fallback={<Loader />} ><Vertical /></Suspense>
-                    <Suspense fallback={<Loader />} ><Cauroselproduct data={mostViewProduct} product={true} /></Suspense>
-                    <Suspense fallback={<Loader />} ><Cauroselstore data={mostViewStore} store={true} /></Suspense>
-                    <Suspense fallback={<Loader />} ><Cauroselbrand data={offersBrand} offer={true} /></Suspense>
+
                     </div>
                 </div>
             </div>
