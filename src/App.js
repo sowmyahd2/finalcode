3988c6e87e29517adde539ed293a2a4e4f685f96
pickupinstore <developer@pickupinstore.com>
2024-11-29 15:39:41 +0530

    sa

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
       try {
         const response = await fetch('http://15.206.201.95/reactapi/public/v1/products/mostview/mysore');
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error('Networkds response was not ok');
         }
         const data = await response.json();
         console.log(data);
