0cc05db28ec3b34ee992a31a9bf4ed87e8fbc506
pickupinstore <developer@pickupinstore.com>
2024-12-08 19:46:41 +0530

    dsds

--- src/Config/Api.js
+++ src/Config/Api.js
@@ -21,7 +21,7 @@ class Api {
     }
     static xhr(route, params, verb) {
         
-       const host = "http://13.233.129.134/reactapi/index.php/" 
+       const host = "https://api.pickupinstore.com/reactApi/public/index.php/v1/" 
         const url = `${host}${route}`;
         const options = Object.assign({ method: verb }, params ? { body: JSON.stringify(params) } : null, 
     );
